83f50fe1645646dbc5e8173af1de89537b539f4a
herokingsley <410638015@qq.com>
2016-11-11 00:02:28 +0800

    推送代码

---
+++ smart-framework/src/main/java/org/herokingsley/framework/ConfigConstant.java
@@ -0,0 +1,19 @@
+package org.herokingsley.framework;
+
+/**
+ * 提供相关配置项常量
+ * Created by Administrator on 2016/11/1.
+ */
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRVIER = "smart.framework.jdbc.drvier";
+    String JDBC_URL = "smart.framework.jdbc.url";
+    String JDBC_USERNAME = "smart.framework.jdbc.username";
+    String JDBC_PASSWORD = "smart.framework.jdbc.password";
+
+    String APP_BASE_PACKAGE = "smart.framework.jdbc.password";
+    String APP_JSP_PATH = "smart.framework.app.jsp_path";
+    String APP_ASSET_PATH = "smart.framework.app.asset_path";
+}
---
+++ smart-framework/src/main/java/org/herokingsley/framework/util/StringUtil.java
@@ -0,0 +1,32 @@
+package org.herokingsley.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串工具类
+ * <br/>Author: her0kings1ey
+ * <br/>Date: 2016/8/7
+ */
+public final class StringUtil {
+
+    /**
+     * 判断字符串是否为空
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str){
+        if(str != null){
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否非空
+     * @param str
+     * @return
+     */
+    public static boolean isNotEmpty(String str){
+        return !isEmpty(str);
+    }
+}
