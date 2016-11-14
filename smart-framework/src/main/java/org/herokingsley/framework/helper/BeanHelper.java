ad0235f973d6e6929f897f4155f831e31ec4670b
herokingsley <410638015@qq.com>
2016-11-15 00:16:57 +0800

    反射类

---
+++ smart-framework/src/main/java/org/herokingsley/framework/helper/BeanHelper.java
@@ -0,0 +1,51 @@
+package org.herokingsley.framework.helper;
+
+import org.herokingsley.framework.util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Bean助手类
+ * @author Administrator
+ * @date 2016/11/14
+ */
+public final class BeanHelper {
+
+    /**
+     * 定义Bean映射（用于存放Bean类与Bean实例的映射关系）
+     */
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    static{
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for(Class<?> beanClass: beanClassSet){
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    /**
+     * 获取Bean映射
+     * @return
+     */
+    public static Map<Class<?>, Object> getBeanMap(){
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取Bean实例
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> cls){
+         if(!BEAN_MAP.containsKey(cls)){
+             throw new RuntimeException("can not get bean by class: " + cls);
+         }
+         return (T)BEAN_MAP.get(cls);
+    }
+
+
+}
